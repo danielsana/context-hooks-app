c417e9cfda597cc069255e31b7e406837d0952a1
dansana <danielsana60@gmail.com>
2024-06-19 22:17:53 +0300

    new

---
+++ src/components/NewBookForm.js
@@ -0,0 +1,28 @@
+import React,{useContext,useState} from 'react'
+import { BookContext } from '../contexts/BookContext';
+
+const NewBookForm = () => {
+    const {addBook}=useContext(BookContext) 
+
+    const [title, setTitle] = useState('')
+
+    const [author, setAuthor] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addBook(title,author)
+        setTitle('')
+        setAuthor('')
+    }
+    return ( 
+        <form>
+            <input type="text" placeholder='book title' value={title} onChange={(e) => setTitle(e.target.value)} required/>
+            <input type="text" placeholder='author' value={author} onChange={(e) => setAuthor(e.target.value)} required/>
+            <button type='submit' onClick={handleSubmit}> add book</button>
+
+            
+        </form>
+     );
+}
+ 
+export default NewBookForm;
